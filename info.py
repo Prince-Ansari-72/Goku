b0c9f040e27e679b2ea504d1f039eb6c5c02f14c
Prince-Ansari-72 <140832335+Prince-Ansari-72@users.noreply.github.com>
2023-09-28 01:40:56 +0530

    Update info.py

--- info.py
+++ info.py
@@ -43,8 +43,8 @@ SUPPORT_CHAT_ID = int(support_chat_id) if support_chat_id and id_pattern.search(
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", False))
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://gokurobotbot:gokurobotbot@cluster0.guht5nl.mongodb.net/?retryWrites=true&w=majority")
-DATABASE_NAME = environ.get('DATABASE_NAME', "telegram1")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://armnmd639:armnmd639@cluster0.54pgwjy.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_NAME = environ.get('DATABASE_NAME', "telegram2")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
 # Others
